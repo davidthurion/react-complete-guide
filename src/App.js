db86e14ea8e97564004523016b6465492abbffe4
David <davidthurion@gmail.com>
2018-10-03 11:48:36 +0200

    feat(class): working with state to add dynamic content

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Max",
+        age: 28
+      },
+      {
+        name: "David",
+        age: 34
+      },
+      {
+        name: "Elza",
+        age: 30
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>This is react!</h1>
-        <Person name="Max" age="28" />
-        <Person name="David" age="34">
+        <button>Switch names</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My hobbies: Climbing
         </Person>
-        <Person name="Elza" age="30" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement(
