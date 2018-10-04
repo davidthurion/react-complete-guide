c63c2bfec201cd6e46f07f6fbaee6439d1bf090d
David <davidthurion@gmail.com>
2018-10-04 08:54:52 +0200

    refactor: outsourcing the condition from the return and only add a reference

--- src/App.js
+++ src/App.js
@@ -76,39 +76,39 @@ class App extends Component {
       cursor: "pointer"
     };
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, "André")}
+            changed={this.nameChangedHandler}
+          >
+            My hobbies: Climbing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>This is react!</h1>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle persons
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "André")}
-              changed={this.nameChangedHandler}
-            >
-              My hobbies: Climbing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
-    // return React.createElement(
-    //   "div",
-    //   null,
-    //   React.createElement("h1", { className: "App" }, "This is react!!")
-    // );
   }
 }
 
